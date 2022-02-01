b827a431b101ea14c4117c9b289043c33aec7d5e
rizkiwibu <rizkiku689@gmail.com>
2022-02-01 19:32:49 +0700

    ryubotz

---
+++ ryudata/sewa.js
@@ -0,0 +1,11 @@
+exports.sewa = () => {
+	return`𝗢 𝗣 𝗘 𝗡  𝗦 𝗘 𝗪 𝗔  𝗕 𝗢 𝗧
+
+• *1 Minggu : 5.000 ( 5K )*
+• *1 Bulan : 17.000 ( 17K )*
+• *Permanen : 25.000 ( 25K )*
+• *Join Murid Bot : 20.000 ( 20K )*
+
+Minat Ketik .owner
+`
+}
